601fec1987f50a4217538c5b704dfa6eaa7616dd
Prakhar Dubey <prakhar.dubey1207@gmail.com>
2025-05-27 10:57:37 +0530

    User Registration Done

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,17 +1,34 @@
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
-const cors=require('cors');
-const express=require('express');
-const app=express();
-const connectToDB=require('./DB/db');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+const userRoutes = require('./Routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
-
-connectToDB();
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the API');
+    res.send('Hello World');
 });
+
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+
+
 
 module.exports = app;
